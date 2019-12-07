556a3dd6d1213190815ef35ddd1d51aa8ce5d883
jmacfarland <macfarland.jamison@gmail.com>
2019-12-07 15:39:26 -0500

    clean up timeline, move most functions to utils

--- timeline.py
+++ timeline.py
@@ -3,32 +3,17 @@ import tweepy
 import json
 
 from models import UserType, User, Tweet
-from utils import remove_emoji
+from utils import Utils
 
 # emoji unicode causes the program to crash, attempted this solution:
 # https://stackoverflow.com/questions/47436649/trying-to-extract-tweets-with-tweepy-but-emojis-make-it-crash
 # but it didnt' work, instead went with outright removing the emojis as shown here:
 # https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-pytho
 
-def load_credentials():
-    with open("credentials.txt") as creds_file:
-        return json.load(creds_file)
 
-def get_user(api, username, user_type=UserType.UNKNOWN):
-    tweets_cursor = tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode="extended")
-    return User(tweets_cursor, user_type)
 
 if __name__ == "__main__":
     username = "StadiaFan" # the twitter handle of an account to scrub the timeline of
-
-    creds = load_credentials()
-    auth = tweepy.OAuthHandler(creds['key'], creds['key_secret'])
-    auth.set_access_token(creds['token'], creds['token_secret'])
-    api = tweepy.API(auth)
-
-    if not api.verify_credentials():
-        print('Bad credentials!')
-        sys.exit(1)
-
-    user = get_user(api, username)
+    api = Utils.get_api()
+    user = Utils.get_user(api, username)
     print(user)
--- utils.py
+++ utils.py
@@ -1,13 +1,34 @@
 import re
+import json
+import tweepy
 
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           u"\U0001F900-\U0001F9FF"  # Twitter icons?
-                           "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
+class Utils:
+    def remove_emoji(string):
+        emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001F900-\U0001F9FF"  # Twitter icons?
+                               "]+", flags=re.UNICODE)
+        return emoji_pattern.sub(r'', string)
+
+    def load_credentials():
+        with open("credentials.txt") as creds_file:
+            return json.load(creds_file)
+
+    def get_user(api, username, user_type=UserType.UNKNOWN):
+        tweets_cursor = tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode="extended")
+        return User(tweets_cursor, user_type)
+
+    def get_api():
+        creds = load_credentials()
+        auth = tweepy.OAuthHandler(creds['key'], creds['key_secret'])
+        auth.set_access_token(creds['token'], creds['token_secret'])
+        api = tweepy.API(auth)
+        if not api.verify_credentials():
+            print('Bad credentials!')
+            sys.exit(1)
+        return api
